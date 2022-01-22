6ff33ca51d167d48dcee47d399f4d18b773fb5ee
irvandindaprakoso <irvandindaprakoso@gmail.com>
2022-01-22 11:18:40 +0700

    LeetCode: contains duplicate

---
+++ LeetCode/containsDuplicate.py
@@ -0,0 +1,7 @@
+def containsDuplicate(self, nums: List[int]) -> bool:
+        unique = set()
+        for i in nums:
+            if i in unique:
+                return True
+            unique.add(i)
+        return False
