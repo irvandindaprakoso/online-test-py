dfe2fbe179dd264b703ef3358f3a8f8cb06a01fc
irvandindaprakoso <irvandindaprakoso@gmail.com>
2022-03-11 16:43:39 +0700

    LeetCode: product Except self

--- LeetCode/productExceptSelf.py
+++ LeetCode/productExceptSelf.py
@@ -2,16 +2,13 @@ nums = [1,2,3,4]
 # nums = [-1, 1, 0, -3, 3]
 res = [1] * (len(nums))
 prefix = 1
+postfix = 1
+
 for i in range(len(nums)):
     res[i] = prefix
     prefix *= nums[i]
-    print(i, res[i], nums[i], prefix)
-print (res)
-postfix = 1
-for i in range(len(nums) -1, -1, -1):
-    res[i] *= postfix
-    postfix *= nums[i]
-    print(i, res[i], nums[i], postfix)
+    res[~i] *= postfix
+    postfix *= nums[~i]
 
 print (res)
 
