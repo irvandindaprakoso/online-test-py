06d1edf4de42637fdf7d67d78ebd254369246210
irvandindaprakoso <irvandindaprakoso@gmail.com>
2023-08-18 11:50:25 +0800

    leetcode: longest common prefix

---
+++ LeetCode/longestCommonPrefix.py
@@ -0,0 +1,12 @@
+def longest_common_prefix(strs):
+    res = ""
+
+    for i in range(len(strs[0])):
+        for s in strs:
+            if i == len(s) or s[i] != strs[0][i]:
+                return res
+        res+= strs[0][i]
+    return res
+
+strs= ["flower","flow","flight"]
+print(longest_common_prefix(strs))
